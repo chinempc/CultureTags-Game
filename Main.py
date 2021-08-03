7a93340494f7d4b5deb1d12fd53b101bec550b48
Anas El Yousfi <anaselyousfi@gmail.com>
2021-08-03 00:27:35 -0400

    graphic instructions

--- Main.py
+++ Main.py
@@ -72,6 +72,8 @@ from CultCards import tabulate
 import time
 import os
 import re
+import pygame
+
 class Player:
 	def __init__(self):
 		self.__Name="Guest"
@@ -119,9 +121,57 @@ class GameManager():
 		self.Players={}
 		self.PresentedCard=None
 	def Instruction(self):
-		with open('gameInstructions.txt', 'r') as f:
+		'''with open('gameInstructions.txt', 'r') as f:
 			file_contents = f.read()
-			print (file_contents)
+			print (file_contents)'''
+
+pygame.init()
+
+
+SIZE = WIDTH, HEIGHT = (1024, 720)
+FPS = 30
+screen = pygame.display.set_mode(SIZE, pygame.RESIZABLE)
+clock = pygame.time.Clock()
+
+
+textfile = open('gameInstructions.txt', 'r')
+lines = textfile.readlines()
+textInput = ''
+for i in lines:
+  textInput = textInput + i
+
+def blit_text(surface, text, pos, font, color=pygame.Color('black')):
+    words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+    space = font.size(' ')[0]  # The width of a space.
+    max_width, max_height = surface.get_size()
+    x, y = pos
+    for line in words:
+        for word in line:
+            word_surface = font.render(word, 0, color)
+            word_width, word_height = word_surface.get_size()
+            if x + word_width >= max_width:
+                x = pos[0]  # Reset the x.
+                y += word_height  # Start on new row.
+            surface.blit(word_surface, (x, y))
+            x += word_width + space
+        x = pos[0]  # Reset the x.
+        y += word_height  # Start on new row.
+
+
+text = textInput
+font = pygame.font.SysFont('Calibri', 12)
+
+while True:
+
+    dt = clock.tick(FPS) / 1000
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    screen.fill(pygame.Color('yellow'))
+    blit_text(screen, text, (20, 20), font)
+    pygame.display.update()
 
 	def Menu(self):
 		Online=True
